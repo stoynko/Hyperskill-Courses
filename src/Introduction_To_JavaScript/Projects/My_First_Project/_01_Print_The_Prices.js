8998522a2cde3aa97af1bc487a1b6fbeabe0d785
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-06-05 17:59:39 +0300

    [05/06/2025]

---
+++ src/Introduction_To_JavaScript/Projects/My_First_Project/_01_Print_The_Prices.js
@@ -0,0 +1,19 @@
+/*Description:
+You've recently opened a new corner shop.
+It is relatively small: it contains only a bubblegum, toffee, ice cream, milk chocolate, doughnuts, and pancakes.
+
+The first version of the program will display a list of all the products with their prices.
+
+Objectives:
+    • Print the Prices: line;
+    • Print the item names and prices from the Example below. Your output format should follow it.
+    • Use console.log() to get the desired output. */
+
+console.log("Prices:")
+console.log("Bubblegum: $2")
+console.log("Toffee: $0.2")
+console.log("Ice cream: $5")
+console.log("Milk chocolate: $4")
+console.log("Doughnut: $2.5")
+console.log("Pancake: $3.2")
+
